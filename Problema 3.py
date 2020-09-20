1279b43fcc0a743bf4383d9daec177b66f23abee
SinigribovS <71001180+SinigribovS@users.noreply.github.com>
2020-09-20 17:12:47 +0300

    Add files via upload

---
+++ Problema 3.py
@@ -0,0 +1,13 @@
+x=input("Lungimea laturii:  ")
+a=int(x)
+
+perimetru= 4*a
+aria_suprafetelor= (a**2)*6
+volumul= a**3
+
+print("Perimetrul:  ")
+print(perimetru)
+print("Aria totala: ")
+print(aria_suprafetelor)
+print("Volumul cubului: ")
+print(volumul)
