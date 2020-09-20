70540d0f3c40c04744c64ec6b09da0cae1377049
SinigribovS <71001180+SinigribovS@users.noreply.github.com>
2020-09-20 14:12:26 +0300

    Add files via upload

---
+++ Problema 2.py
@@ -0,0 +1,23 @@
+x=input("Primul numar  " )
+y=input("al doilea numar  " ) 
+a=int(x)
+b=int(y)
+suma = a+b
+diferenta = a-b
+catul = a//b
+restul = a%b
+produsul = a*b
+puterea = a**b
+
+print("Suma: ")
+print(suma)
+print("Diferenta: ")
+print(diferenta)
+print("Catul impartirii: ")
+print(catul)
+print("Restul impartirii: ")
+print(restul)
+print("Produsul: ")
+print(produsul)
+print("Ridicarea la putere: ")
+print(puterea)
