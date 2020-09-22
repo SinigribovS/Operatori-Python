c2d24cf22cbde6f3932ceb7bb6ef0a80d707db36
SinigribovS <71001180+SinigribovS@users.noreply.github.com>
2020-09-22 19:45:51 +0300

    Add files via upload

---
+++ Problema 5.py
@@ -0,0 +1,8 @@
+print("Tabelul de adevăr al functiilor logice: AND, OR, NOT")
+x=True
+y=False
+print("x     y       x and y     x or y     not x      not y")
+print( x,"", y ," ", x and y,"     ", x or y,"     " ,not x,"    " ,not y)
+print( y, x, "  ", y and x,"     ", y or x,"     ", not y,"     ",not x)
+print( y, y,  " ", y and y,"     ", y or y,"    ", not y,"     ",not y)
+print( x,"", x, "  ", x and x,"      ", x or x,"     ", not x,"    ",not x)
